cbcba18cad520920f36b7c1cbb27917b38d3724d
oumaimahajej <hajejoumaima@gmail.com>
2020-02-06 10:44:17 +0100

    add Prefab

---
+++ LineGame/Assets/Scripts/LineGenerator.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LineGenerator : MonoBehaviour
+{
+    Rigidbody rb;
+    public float speed = 50f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = this.GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        
+    }
+}
