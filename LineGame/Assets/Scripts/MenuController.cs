b05ee9d1a91586915823a9b6e9dd8fbe8237bc8f
oumaimahajej <hajejoumaima@gmail.com>
2020-03-03 13:22:42 +0100

    buttons
    eli fi game structure kolhom des scene ??

---
+++ LineGame/Assets/Scripts/MenuController.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public void startGame()
+    {
+        SceneManager.LoadScene(2);
+
+    }
+    public void BackMenu()
+    {
+        SceneManager.LoadScene(1);
+
+    }
+}
